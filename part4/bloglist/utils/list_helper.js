2dcdd452aca2d7d26deffbd007dff2d34746adc2
elhussler <animanic@hotmail.co.uk>
2024-06-06 16:23:26 +0100

    exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -5,8 +5,22 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
-  
+
+const favouriteBlog = (blogs) => {
+    const favouriteBlog = blogs.reduce((prev, current) => prev && prev.likes > current.likes ? prev : current, null)
+    
+    if (!favouriteBlog)
+        return null
+
+    return  {
+        title: favouriteBlog.title,
+        author: favouriteBlog.author,
+        likes: favouriteBlog.likes
+    }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
