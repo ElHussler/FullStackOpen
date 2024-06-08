bbd8dde753dc0612ce381f1216b42822392ff6b5
elhussler <animanic@hotmail.co.uk>
2024-06-08 15:59:06 +0100

    exercise 4.13

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -17,65 +17,66 @@ beforeEach(async () => {
     await Promise.all(promiseArray)
 })
 
-test('GET returns correct number of blog posts in json format', async () => {
-    const response = await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+describe('GET blog posts', () => {
+    test('GET returns correct number of blog posts in json format', async () => {
+        const response = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
-})
+        assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
 
-test('unique id blog post property is named id', async () => {
-    const response = await api
-        .get('/api/blogs')
-        .expect(200)
-    
-    const blogPost = response.body[0]
-    
-    assert.strictEqual(Object.keys(blogPost).includes('id'), true)
-})
-
-test('POST creates new blog post', async () => {
-    const newBlog = {
-        title: "ADDED BY TEST",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 69,
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    const blogsInDb = (await Blog.find({})).map(blog => blog.toJSON())
-    const titles = blogsInDb.map(b => b.title)
-
-    assert.strictEqual(blogsInDb.length, helper.initialBlogs.length + 1)
-    assert(titles.includes('ADDED BY TEST'))
-})
-
-test('if likes property is missing default to zero', async () => {
-    const newBlog = {
-        title: "ADDED BY TEST WITHOUT LIKES PROPERTY",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/"
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-    
-    const blogSavedInDb = (await Blog.find({ title: newBlog.title })).map(blog => blog.toJSON())
-
-    assert.strictEqual(blogSavedInDb[0].likes, 0)
+    test('unique id blog post property is named id', async () => {
+        const response = await api
+            .get('/api/blogs')
+            .expect(200)
+        
+        const blogPost = response.body[0]
+        
+        assert.strictEqual(Object.keys(blogPost).includes('id'), true)
+    })
 })
 
 describe('POST new blog post', () => {
+    test('POST creates new blog post', async () => {
+        const newBlog = {
+            title: "ADDED BY TEST",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 69,
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsInDb = (await Blog.find({})).map(blog => blog.toJSON())
+        const titles = blogsInDb.map(b => b.title)
+
+        assert.strictEqual(blogsInDb.length, helper.initialBlogs.length + 1)
+        assert(titles.includes('ADDED BY TEST'))
+    })
+
+    test('if likes property is missing default to zero', async () => {
+        const newBlog = {
+            title: "ADDED BY TEST WITHOUT LIKES PROPERTY",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/"
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        
+        const blogSavedInDb = (await Blog.find({ title: newBlog.title })).map(blog => blog.toJSON())
+
+        assert.strictEqual(blogSavedInDb[0].likes, 0)
+    })
     test('if title property is missing return 400 bad request', async () => {
         const newBlog = {
             author: "Michael Chan WITHOUT A BLOG TITLE",
@@ -99,6 +100,36 @@ describe('POST new blog post', () => {
             .post('/api/blogs')
             .send(newBlog)
             .expect(400)
+    })
+})
+
+describe('DELETE single blog post', () => {
+    test('succeeds with 204 code if id is valid', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+
+        await api
+          .delete(`/api/blogs/${blogToDelete.id}`)
+          .expect(204)
+        
+        const blogsAtEnd = await helper.blogsInDb()
+
+        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+        const titles = blogsAtEnd.map(b => b.title)
+        assert(!titles.includes(blogToDelete.title))
+    })
+    test('fails with 204 code if id is invalid', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogIdToDelete = await helper.nonExistingId()
+
+        await api
+          .delete(`/api/blogs/${blogIdToDelete}`)
+          .expect(204)
+        
+        const blogsAtEnd = await helper.blogsInDb()
+
+        assert.strictEqual(blogsAtEnd.length, blogsAtStart.length)
     })
 })
 
--- part4/bloglist/tests/blog_test_helper.js
+++ part4/bloglist/tests/blog_test_helper.js
@@ -51,6 +51,21 @@ const initialBlogs = [
     }  
 ]
 
+const nonExistingId = async () => {
+    const blog = new Blog({ title: 'i dont exist', url: 'fake' })
+    await blog.save()
+    await blog.deleteOne()
+
+    return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+}
+
 module.exports = {
-    initialBlogs
+    initialBlogs,
+    blogsInDb,
+    nonExistingId
 }
