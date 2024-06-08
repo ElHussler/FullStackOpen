58961f7e003fbab5acf1f49c5750401035060d22
elhussler <animanic@hotmail.co.uk>
2024-06-08 13:44:43 +0100

    exercise 4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./blog_test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initialBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+test('GET returns correct number of blog posts in json format', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
