d709760320995e7b34e650f679079a2253cf35c9
elhussler <animanic@hotmail.co.uk>
2024-06-17 18:33:55 +0100

    errors

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -48,8 +48,6 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: 'expected `username` to be unique' })
   } else if (error.name ===  'JsonWebTokenError') {
     return response.status(401).json({ error: 'token invalid' })
-  } else if (error.name ===  'Internal Server Error') {
-    return response.status(500).json({ error: 'token invalid' })
   }
 
   next(error)
